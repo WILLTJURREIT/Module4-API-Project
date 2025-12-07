2b8d9c7247264c4bfb6f7b6e904c3e511eda0c5e
WILLTJURREIT <wjurreit@northislandcollege.ca>
2025-12-07 13:41:22 -0800

    Add code to set up for axios in app.js
    Check in console log

--- public/app.js
+++ public/app.js
@@ -10,19 +10,21 @@ const keywordBtn = document.getElementById("keyword-btn");
 const sectionSelect = document.getElementById("section-select");
 const noResultsMsg = document.getElementById("no-results-message");
 
-
 //Fetch the top stories 
 async function getTopStories(section) {
   const url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`;
 
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const response = await axios.get(url);
+    const data = response.data;
+    renderArticles(data.results);
+    noResultsMsg.classList.add("hidden");
+    noResultsMsg.textContent = "";
     console.log("API DATA LOADED:", data);
   } catch (err) {
     console.error("Error fetching NYT data:", err);
+    articlesDiv.innerHTML = "<p>Failed to load stories. Please try again.</p>";
   }
 }
+console.log("error not happening", getTopStories());
 
-getTopStories(); 
-alert(getTopStories());
