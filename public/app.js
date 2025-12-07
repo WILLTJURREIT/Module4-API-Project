92766f93692ff231865c37a01501dad8f5d72185
WILLTJURREIT <wjurreit@northislandcollege.ca>
2025-12-07 13:29:43 -0800

    Add styles for navigation and btn
    Get all elements by ID into reference variables in app.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,18 @@
 "use strict";
 
-
+// API Key 
 const API_KEY = "VM76EAB8OgnfTyrMrLa14ADwIa2wUNB2";
 
+// references 
+const articlesDiv = document.getElementById("articles");
+const keywordInput = document.getElementById("keyword");
+const keywordBtn = document.getElementById("keyword-btn");
+const sectionSelect = document.getElementById("section-select");
+const noResultsMsg = document.getElementById("no-results-message");
 
 
-
-async function getTopStories(section = "world") {
+//Fetch the top stories 
+async function getTopStories(section) {
   const url = `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${API_KEY}`;
 
   try {
